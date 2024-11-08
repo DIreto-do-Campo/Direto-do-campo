ff531e124a3a56b666130c03b98bbec18ee8add1
Geraldnvs <86506788+Geraldnvs@users.noreply.github.com>
2024-11-08 16:32:39 -0300

    Add files via upload

--- src/components/EmptyCart.tsx
+++ src/components/EmptyCart.tsx
@@ -1,14 +1,16 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const EmptyCart = () => {
   return (
     <div className="flex flex-col items-center justify-center h-screen bg-gray-100">
       <h1 className="text-3xl font-bold mb-4 text-black">Seu carrinho está vazio!</h1>
       <p className="text-lg mb-6 text-black">Adicione produtos para finalizar sua compra.</p>
-      <a href="/"><button className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded">
-        Continuar comprando
-      </button>
-      </a>
+      <Link to="/">
+        <button className="bg-hunterGreen hover:bg-green-900 text-white font-bold py-2 px-4 rounded">
+          Continuar comprando
+        </button>
+      </Link>
     </div>
   );
 };
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import { UserCircleIcon, ShoppingCartIcon, MapIcon, InformationCircleIcon } from '@heroicons/react/24/outline';
 import { ChevronDownIcon } from '@heroicons/react/24/solid';
 
@@ -6,48 +7,45 @@ const Navbar = () => {
   return (
     <nav className="bg-hunterGreen h-16 text-white px-6 py-4 flex justify-between items-center rounded-t-[19px]">
       {/* Logo */}
-      <a href="/">
-      <div className="flex items-center space-x-2">
-        <img src="/LOGO.png" alt="Logo" className="h-10" />
-      </div>
-      </a>
+      <Link to="/">
+        <div className="flex items-center space-x-2">
+          <img src="/LOGO.png" alt="Logo" className="h-10" />
+        </div>
+      </Link>
 
       {/* Navegação */}
-      
       <div className="flex space-x-8">
-      <a href="/explore" className=" text-white">
+        <Link to="/explore" className="text-white">
           <span className="flex items-center">
             <MapIcon className="h-5 w-5 mr-1" /> {/* Ícone de mapa */}
             Explore
           </span>
-        </a>
-        <a href="/About" className=" text-white">
+        </Link>
+        <Link to="/About" className="text-white">
           <span className="flex items-center">
-            <InformationCircleIcon className="h-5 w-5 mr-1" /> {/* Ícone de mapa */}
+            <InformationCircleIcon className="h-5 w-5 mr-1" /> {/* Ícone de informação */}
             Sobre o Projeto
           </span>
-        </a>
-        <a href="/Farm" className="text-white">
+        </Link>
+        <Link to="/Farm" className="text-white">
           Venda com a gente
-        </a>
+        </Link>
       </div>
 
       {/* Ícones do usuário e carrinho */}
-      <div className="flex items-center space-x-6 ">
+      <div className="flex items-center space-x-6">
         {/* Ícone de usuário */}
         <div className="bg-white rounded-full w-10">
-          <a href="/Login" className="relative flex items-center">
+          <Link to="/Login" className="relative flex items-center">
             <UserCircleIcon className="h-6 w-6 text-hunterGreen ml-[3px]" /> {/* Ícone de usuário */}
             <ChevronDownIcon className="h-6 w-6 text-hunterGreen ml-1" />
-          </a>
-
+          </Link>
         </div>
 
         {/* Ícone de carrinho */}
-        <a href="/cart" className="relative">
+        <Link to="/cart" className="relative">
           <ShoppingCartIcon className="h-6 w-6 text-white" /> {/* Ícone de carrinho */}
-
-        </a>
+        </Link>
       </div>
     </nav>
   );
