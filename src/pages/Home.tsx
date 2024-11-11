ed4980f95ba2da2ade9c4156b18b8964be2bda9b
Geraldnvs <86506788+Geraldnvs@users.noreply.github.com>
2024-11-10 22:31:19 -0300

    Add files via upload

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -4,12 +4,13 @@ import Navbar from '../components/Navbar';
 import ProductGrid from '../components/ProductGrid';
 import Footer from '../components/Footer';
 import { CarrinhoContext } from '../contexts/context';
+import { Link } from 'react-router-dom';
 
 
 
 const Home = () => {
-    const GlobalState= useContext(CarrinhoContext)
-    console.log('ola mundo',GlobalState)
+    const GlobalState = useContext(CarrinhoContext)
+    console.log('ola mundo', GlobalState)
     return (
 
         <div className="max-w-[1440px] mx-auto bg-cinereous/20 rounded-[19px] min-h-screen flex flex-col">
@@ -50,11 +51,11 @@ const Home = () => {
             {/* Grid de produtos */}
             <div className="container mx-auto bg-white rounded-[15px] py-4 mt-12 flex justify-center w-[90%] relative">
                 <ProductGrid />
-                
+
                 {/* Botão de seta para o carrinho */}
-                
+
             </div>
-            
+
             <div className="bg-amethyst relative text-white container mx-auto rounded-[15px] py-8 mt-4 w-[90%] px-4">
                 <div className="flex items-center">
                     <img src="/dolarcoin.png" alt="Ícone Dólar" className="w-6 h-auto" />
@@ -73,11 +74,15 @@ const Home = () => {
                         alt="Agricultura Familiar"
                     />
                 </div>
+
                 <div className="w-1/2 left-[52%] absolute top-4">
                     <span className='bg-white p-1 text-sm rounded-sm font-medium text-hunterGreen'>Top Farm</span>
-                    <h2 className='my-4 text-lg font-semibold'>Materia sobre nossos fornecedores</h2>
-                    <p>Contando a historia de vida de uma das nossas agricultoras</p>
+                    <h2 className='my-4 text-lg font-semibold'>VIDEO PITCH</h2>
+                    <Link to={'https://youtu.be/REqr2nS-_Ew?si=_h1laMTJQ5t14nwL'} target="_blank" className="text-blue-500 underline">
+                        Esse é o link do vídeo pitch no YouTube
+                    </Link>
                 </div>
+
             </div>
 
             <Footer />
